1ac78aa34a4729a98d6ee3661f6596d40ee502c7
SILVA Alonso <alonso.silva@gmail.com>
2023-09-21 18:22:55 +0200

    Rephrase

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title("Ingresos en Chile")
 
 st.markdown("A continuación se presentan los datos de la Encuesta Suplementaria de Ingresos que puedes descargar desde [este enlace]('https://www.ine.gob.cl/estadisticas/sociales/ingresos-y-gastos/encuesta-suplementaria-de-ingresos').")
 
-st.markdown("Hemos seleccionado sólo las columnas relevantes para esta aplicación pero puedes descargarla completa seleccionando 'Import' y luego 'Import Files'.")
+st.markdown("Hemos seleccionado sólo las columnas relevantes para esta aplicación pero puedes descargar aquí la encuesta completa seleccionando 'Import' y luego 'Import Files'.")
 
 st.markdown("Sigue las instrucciones para obtener el ingreso medio de las personas ocupadas en Chile.")
 
@@ -34,17 +34,17 @@ CHECKS_AND_ERRORS = [
     (
         lambda df: (df["ocup_ref"].isnull().sum() > 0),
         '(1/9) Por favor selecciona sólo las personas ocupadas con más de 1 mes en el empleo actual.',
-        "Para hacerlo selecciona el ícono de filtro de la columna 'ocup_ref', y luego agregando el filtro '=' a 1."
+        "Para hacerlo selecciona el ícono de filtro de la columna 'ocup_ref', y luego agrega el filtro '=' a 1."
     ),
     (
         lambda df: len(df.columns) < 5,
         "(2/9) Por favor agrega una nueva columna.",
-        "Para hacerlo selecciona la columna 'sexo' y haz click en 'Add Col' en el menú"
+        "Para hacerlo selecciona la columna 'sexo' y haz click en 'Add Col' en el menú."
     ),
     (
         lambda df: "ingreso_ponderado" not in list(df.columns),
         "(3/9) Cambia el nombre de la nueva columna a 'ingreso_ponderado'.",
-        "Para hacerlo haz doble click en el nombre de la columna"
+        "Para hacerlo haz doble click en el nombre de la columna."
     ),
     (
         lambda df: df.iloc[0,4] != data.iloc[0,0]*data.iloc[0,1],
@@ -54,27 +54,27 @@ CHECKS_AND_ERRORS = [
     (
         lambda df: len(df.columns) < 6,
         "(5/9) Por favor agrega una nueva columna.",
-        "Para hacerlo selecciona la columna 'ingreso_ponderado' y haz click en 'Add Col' en el menú"
+        "Para hacerlo selecciona la columna 'ingreso_ponderado' y haz click en 'Add Col' en el menú."
     ),
     (
         lambda df: "resultados" not in list(df.columns),
-        "(6/9) Cambia el nombre de la nueva columna a 'resultados'",
-        "Para hacerlo haz doble click en el nombre de la columna"
+        "(6/9) Cambia el nombre de la nueva columna a 'resultados'.",
+        "Para hacerlo haz doble click en el nombre de la columna."
     ),
     (
         lambda df: np.round(df.iloc[0,5]) != np.round(data['ingreso_ponderado'].sum()),
         "(7/9) Calcula el total de ingresos ponderados por el factor de expansión.",
-        "Para hacerlo deselecciona 'Edit entire column' y agrega la ecuación '=sum(ingreso_ponderado1\:ingreso_ponderado93103)' en la célula 1 de la columna 'resultados'"
+        "Para hacerlo agrega la ecuación '=sum(ingreso_ponderado1\:ingreso_ponderado93103)' y **deselecciona 'Edit entire column'** en la celda 1 de la columna 'resultados'."
     ),
     (
         lambda df: np.round(df.iloc[1,5]) != np.round(data[data["ocup_ref"] == 1]["fact_cal_esi"].sum()),
         "(8/9) Calcula el número estimado de personas ocupadas.",
-        "Para hacerlo deselecciona 'Edit entire column' y  agrega la ecuación '=sum(fact_cal_esi1\:fact_cal_esi93103)' en la célula 3 de la columna 'resultados'"
+        "Para hacerlo agrega la ecuación '=sum(fact_cal_esi1\:fact_cal_esi93103)' y **deselecciona 'Edit entire column'** en la celda 3 de la columna 'resultados'."
     ),
     (
         lambda df: np.round(df.iloc[2,5]) != np.round(data['ingreso_ponderado'].sum()/(data[data["ocup_ref"] == 1]["fact_cal_esi"].sum())),
         "(9/9) Calcula el ingreso promedio mensual en Chile.",
-        "Para hacerlo deselecciona 'Edit entire column' y agrega la ecuación '=resultado1/resultado3' en la célula 5 de la columna 'resultados'"
+        "Para hacerlo agrega la ecuación '=resultados1/resultados3' y **deselecciona 'Edit entire column'** en la celda 5 de la columna 'resultados'."
     ),
 ]
 
@@ -99,7 +99,7 @@ def ingreso_promedio(data):
 if checks_passed:
     n_ocupados = df["fact_cal_esi"].sum()
     st.success('Felicitaciones! Lo lograste!', icon="✅")
-    st.write(f"Número de personas ocupadas en Chile es: {n_ocupados:,.0f}".replace(',','.'))
+    st.write(f"El número de personas ocupadas en Chile es: {n_ocupados:,.0f}".replace(',','.'))
     st.write(f"El ingreso medio mensual de los ocupados en Chile es: ${ingreso_promedio(df):,.0f}".replace(',','.'))
     st.write(f"El código Python que generaste para obtener esos resultados es el siguiente:")
 
